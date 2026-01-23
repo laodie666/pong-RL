83028c744aabcebabbf04ee76810bf600a49296d
laodie666 <mhkjordan@gmail.com>
2026-01-23 09:33:34 -0500

    added dropout

--- game.py
+++ game.py
@@ -16,8 +16,10 @@ class PolicyNetwork(nn.Module):
         self.fc1 = nn.Sequential(
             nn.Linear(input_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, hidden_size), 
             nn.ReLU(),
+            nn.Dropout(0.1),
             nn.Linear(hidden_size, output_size),
             nn.Softmax(dim=1)
         )
